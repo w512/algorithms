1b512d7f3cc30f265e79cf73c481efcb6bbb0d4c
Nikolay Blokhin <nikolay@blokhin.us>
2017-01-06 16:28:02 +0100

    More for 'linked list' algorithm

--- linked_list.py
+++ linked_list.py
@@ -16,6 +16,16 @@ class LinkedList:
         self.first = None
         self.last = None
 
+    def __str__(self):
+        if self.first is not None:
+            current = self.first
+            out = 'LinkedList [\n\t{0}\n'.format(current.value)
+            while current.next_node is not None:
+                current = current.next_node
+                out += '\t{0}\n'.format(current.value)
+            return out + ']'
+        return 'LinkedList []'
+
     def clear(self):
         self.__init__()
 
@@ -36,15 +46,43 @@ class LinkedList:
         new_first.next_node = self.first
         self.first = new_first
 
-    def __str__(self):
-        if self.first is not None:
+    def insert(self, position, value):
+        if position == 0 or not self.first:
+            self.prepend(value)
+        else:
+            new_node = Node(value)
+            iter_node = self.first
+            tmp_position = position
+            while tmp_position > 1 and iter_node.next_node:
+                iter_node = iter_node.next_node
+                tmp_position -= 1
+            new_node.next_node = iter_node.next_node
+            iter_node.next_node = new_node
+
+    def delete(self, position):
+        if not self.first:
+            pass
+        elif position == 0:
+            self.first = self.first.next_node
+        else:
+            iter_node = self.first
+            pos = position
+            while pos > 1 and iter_node.next_node:
+                iter_node = iter_node.next_node
+                pos -= 1
+            if iter_node.next_node:
+                iter_node.next_node = iter_node.next_node.next_node
+
+    def reverse(self):
+        if self.first:
+            prev = None
             current = self.first
-            out = 'LinkedList [\n\t{0}\n'.format(current.value)
-            while current.next_node is not None:
-                current = current.next_node
-                out += '\t{0}\n'.format(current.value)
-            return out + ']'
-        return 'LinkedList []'
+            while current:
+                future = current.next_node
+                current.next_node = prev
+                prev = current
+                current = future
+            self.first = prev
 
 
 if __name__ == '__main__':
@@ -64,7 +102,7 @@ if __name__ == '__main__':
     ll.append(n4)      # [234, 1, 55, 98]
     ll.append(n5)      # [234, 1, 55, 98, 7]
 
-    print()
+    print('Variant 1')
     print(ll)
     print()
 
@@ -75,22 +113,30 @@ if __name__ == '__main__':
     ll.append(n4)      # [1, 7, 55, 98]
     ll.append(n1)      # [1, 7, 55, 98, 234]
 
-    print()
+    print('Variant 2')
     print(ll)
     print()
 
     ll.clear()         # []
-    ll.append(n3)      # [55]
-    ll.prepend(n5)     # [7, 55]
-    ll.append(n4)      # [7, 55, 98]
-    ll.prepend(n2)     # [1, 7, 55, 98]
-    ll.append(n1)      # [1, 7, 55, 98, 234]
+    ll.append(n5)      # [7]
+    ll.append(n4)      # [7, 98]
+    ll.prepend(n2)     # [1, 7, 98]
+    ll.append(n1)      # [1, 7, 98, 234]
+    ll.insert(2, n3)   # [1, 7, 55, 98, 234]
 
-    print()
+    print('Variant 3')
     print(ll)
     print()
 
+    ll.delete(2)   # [1, 7, 98, 234]
 
+    print('Variant 4')
+    print(ll)
+    print()
 
+    ll.insert(2, n3)   # [1, 7, 55, 98, 234]
+    ll.reverse()       # [234, 98, 55, 7, 1]
 
-
+    print('Variant 5')
+    print(ll)
+    print()
